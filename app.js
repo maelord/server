eb08e6f7a671b1ede5890eaba63c574afec64413
vaniairnanda <vaniairnanda@gmail.com>
2020-01-16 19:23:54 +0700

    commit 1, image uploader done

---
+++ app.js
@@ -0,0 +1,57 @@
+'use strict'
+//All Dependencies
+const express = require('express')
+const logger = require('morgan')
+
+      //All Route Files
+const routes = require('./routes/index')
+
+      //Express Instance
+const app = express()
+
+//load environment variables with dotenv
+require('dotenv').config()
+
+app.use(require('cors')())
+app.use(logger('dev'))
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send({
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.send({
+    message: err.message,
+    error: {}
+  })
+})
+
+
+app.listen(3000, () => {
+    console.log('app running on port', 3000)
+})
+
+
+module.exports = app
